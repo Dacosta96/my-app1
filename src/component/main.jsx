c3c1b59daea898b50700836d0e58d209aed61cc9
DACOSTA96 <diego.ac9614@gmail.com>
2024-05-14 09:23:25 -0500

    implemetation navigation

--- src/component/main.jsx
+++ src/component/main.jsx
@@ -1,10 +1,14 @@
 import { View } from "react-native";
 import NavigationStack from "./../navigation/NavigationStack";
+import NavigationTab from "../navigation/NavigationTab";
+import NavigationDrawer from "../navigation/NavigationDrawer";
 
 export default function Main() {
   return (
     <View style={{ marginTop: 50, flexGrow: 1 }}>
-      <NavigationStack />
+      {/* <NavigationStack /> */}
+      {/* <NavigationTab /> */}
+      <NavigationDrawer />
     </View>
   );
 }
